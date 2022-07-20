cfdc7116c143c81d028b3544ac42a2e1fb4876bb
Gabriel Alencar <gfalencar02@gmail.com>
2022-07-20 09:05:40 -0300

    Add react website

---
+++ 03-site-estatico/website/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+            Curso AWS com terraform
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/galencar1/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GitHub
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
